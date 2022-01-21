a4c921183631f0932f42c9f0cdb430513e0ffedd
Taylor Boyd <taylor.boyd@oit.edu>
2022-01-20 16:59:08 -0800

    Created the Array2D class. Added a 2D operator array index, a ctor, and cctor.

---
+++ CST211-Lab3-Test/Array2D.h
@@ -0,0 +1,105 @@
+#ifndef ARRAY2D_H
+#define ARRAY2D_H
+#include <iostream>
+
+template<typename T>
+class Array2D
+{
+private:
+	T** m_array;
+	int m_rows;
+	int m_columns;
+public:
+	Array2D() : m_array(nullptr), m_rows(0), m_columns(0) {}
+	Array2D(const int rows, const int columns); // default constructor
+	Array2D(const Array2D& copy); // copy constructor
+	//Array2D& operator =(const Array2D& rhs); // copy assignment
+	~Array2D(); // destructor
+
+	int getRow() const; // getter function for rows
+	int getColumns() const; // getter function for columns
+
+	//void setColumns(int columns); // setter function for columns
+	//void setRows(int rows); // setter function for rows
+	//T& Select(const int desired_row, const int desired_column) const; // select for row and column
+
+   //Row operator[](int index) { return Row(m_array[index]); }
+
+	template<typename T>
+	class Row {
+	public:
+		Row(T* arr) : arr(arr) {}
+		T & operator[](int index) { return arr[index]; }
+	private:
+		T* arr = nullptr;
+	};
+	Row<T> & operator[](int index){ 
+		Row<T> object(m_array[index]);
+		return object;
+	}
+};
+
+
+
+template<typename T>
+inline Array2D<T>::Array2D(const int rows, const int columns)
+{
+	m_rows = rows;
+	m_columns = columns;
+	
+	m_array = new T * [m_rows];
+	for (int i = 0; i < m_rows; ++i) {
+		m_array[i] = new T[m_columns];
+	}
+}
+
+template<typename T>
+inline Array2D<T>::Array2D(const Array2D& copy)
+{
+	m_rows = copy.m_rows;
+	m_columns = copy.m_columns;
+	
+	m_array = new T * [m_rows];
+	for (int i = 0; i < m_rows; ++i) {
+		m_array[i] = new T[m_columns];
+	}
+	// initializes the object with the copied objects rows and columns
+	
+	for (int i = 0; i < m_rows; i++)
+	{
+		for (int j = 0; j < m_columns; j++)
+		{
+			m_array[i][j] = copy.m_array[i][j];
+		}
+	}
+	// copies the values assigned to each of the 2D array elements
+	
+
+	std::cout << "copied!" << std::endl;
+}
+
+template<typename T>
+inline Array2D<T>::~Array2D()
+{
+	for (int i = 0; i < m_rows; ++i) {
+		delete[] m_array[i];
+	}
+	delete[] m_array;
+	m_array = nullptr;
+}
+
+template<typename T>
+inline int Array2D<T>::getRow() const
+{
+	return m_rows;
+}
+
+template<typename T>
+inline int Array2D<T>::getColumns() const
+{
+	return m_columns;
+}
+
+
+
+#endif
